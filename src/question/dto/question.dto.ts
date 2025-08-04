a19b634fbaa35930f59f9895f688745f388fb961
marlonchiu <Knight1898@outlook.com>
2025-08-04 10:01:15 +0800

    4 路由常见功能：query，param和request-body

---
+++ src/question/dto/question.dto.ts
@@ -0,0 +1,6 @@
+//  dto 数据传输对象 data transfer object
+
+export class QuestionDto {
+  readonly title: 'string';
+  readonly desc: 'string';
+}
--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,17 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
+import { QuestionDto } from './dto/question.dto';
 
 @Controller('question')
 export class QuestionController {
   @Get()
-  findAll() {
+  findAll(
+    @Query('keyword') keyword: string,
+    @Query('page') page: number,
+    @Query('pageSize') pageSize: number,
+  ) {
+    console.log(keyword, page, pageSize);
     return {
       list: ['a', 'b', 'c'],
       count: 100,
     };
   }
 
-  @Get('/test')
-  getTest(): string {
-    return 'question test';
+  // get 中的 id 要和 @Param('id')  一致
+  @Get(':id')
+  findOne(@Param('id') uid: string) {
+    console.log(uid);
+    return {
+      id: '1',
+      title: 'title',
+      desc: 'content',
+    };
   }
+
+  @Patch(':id')
+  updateOne(@Param('id') id: string, @Body() questionDto: QuestionDto) {
+    console.log(id, questionDto);
+    return {
+      id,
+      questionDto,
+    };
+  }
+
+  // @Get('/test')
+  // getTest(): string {
+  //   return 'question test';
+  // }
 }
